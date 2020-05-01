18ce6ec712444274841542fd3c1f3b839403a85c
Bruce Allen <bdallen@nps.edu>
2020-04-30 17:55:32 -0700

    replace ray with sonar

--- nps_gazebo_ros_sonar/include/gazebo_plugins/nps_gazebo_ros_sonar.h
+++ nps_gazebo_ros_sonar/include/gazebo_plugins/nps_gazebo_ros_sonar.h
@@ -1,26 +1,5 @@
 /*
- * Copyright (C) 2012-2014 Open Source Robotics Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
-*/
-/*
- * Desc: ros laser controller.
- * Author: Nathan Koenig
- * Date: 01 Feb 2007
- */
-/*
- * Desc: ros sonar controller.
+ * Desc: NPS Ros Sonar controller.
  * Author: Bruce Allen
  * Date: 01 May 2020
  */
@@ -40,18 +19,18 @@
 #include <gazebo/common/Time.hh>
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/sensors/SensorTypes.hh>
-#include <gazebo/plugins/RayPlugin.hh>
+#include <gazebo/plugins/SonarPlugin.hh>
 
 #include <boost/bind.hpp>
 #include <boost/thread.hpp>
 #include <boost/thread/mutex.hpp>
 
-#include <sensor_msgs/PointCloud.h>
+#include <sensor_msgs/Range.h>
 
 namespace gazebo
 {
 
-  class NpsGazeboRosSonar: public RayPlugin
+  class NpsGazeboRosSonar: public SonarPlugin
   {
     /// \brief Constructor
     /// \param parent The parent entity, must be a Model or a Sensor
@@ -65,30 +44,31 @@ namespace gazebo
     public: void Load(sensors::SensorPtr _parent, sdf::ElementPtr _sdf);
 
     /// \brief Update the controller
-    protected: virtual void OnNewLaserScans();
+    protected: virtual void OnNewSonarScans();
 
-    /// \brief Put laser data to the ROS topic
-    private: void PutLaserData(common::Time &_updateTime);
+    /// \brief Put sonar data to the ROS topic
+    private: void PutSonarData(common::Time &_updateTime);
 
     private: common::Time last_update_time_;
 
     /// \brief Keep track of number of connctions
-    private: int laser_connect_count_;
-    private: void LaserConnect();
-    private: void LaserDisconnect();
+    private: int range_connect_count_;
+    private: void RangeConnect();
+    private: void RangeDisconnect();
 
-    // Pointer to the model
+    // \brief Pointer to the model
     private: physics::WorldPtr world_;
+
     /// \brief The parent sensor
     private: sensors::SensorPtr parent_sensor_;
-    private: sensors::RaySensorPtr parent_ray_sensor_;
+    private: sensors::SonarSensorPtr parent_sonar_sensor_;
 
     /// \brief pointer to ros node
     private: ros::NodeHandle* rosnode_;
     private: ros::Publisher pub_;
 
     /// \brief ros message
-    private: sensor_msgs::PointCloud cloud_msg_;
+    private: sensor_msgs::Range range_msg_;
 
     /// \brief topic name
     private: std::string topic_name_;
@@ -96,29 +76,16 @@ namespace gazebo
     /// \brief frame transform name, should match link name
     private: std::string frame_name_;
 
-    /// \brief Gaussian noise
-    private: double gaussian_noise_;
-
-    /// \brief Gaussian noise generator
-    private: double GaussianKernel(double mu,double sigma);
-
     /// \brief A mutex to lock access to fields that are used in message callbacks
     private: boost::mutex lock;
-
-    /// \brief hack to mimic hokuyo intensity cutoff of 100
-    //private: ParamT<double> *hokuyoMinIntensityP;
-    private: double hokuyo_min_intensity_;
-
-    /// update rate of this sensor
-    private: double update_rate_;
 
     /// \brief for setting ROS name space
     private: std::string robot_namespace_;
 
     // Custom Callback Queue
-    private: ros::CallbackQueue laser_queue_;
-    private: void LaserQueueThread();
-    private: boost::thread callback_laser_queue_thread_;
+    private: ros::CallbackQueue sonar_queue_;
+    private: void SonarQueueThread();
+    private: boost::thread callback_sonar_queue_thread_;
 
     // subscribe to world stats
     private: transport::NodePtr node_;
--- nps_gazebo_ros_sonar/src/nps_gazebo_ros_sonar.cpp
+++ nps_gazebo_ros_sonar/src/nps_gazebo_ros_sonar.cpp
@@ -1,26 +1,5 @@
 /*
- * Copyright 2013 Open Source Robotics Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
-*/
-/*
- * Desc: Ros Block Laser controller.
- * Author: Nathan Koenig
- * Date: 01 Feb 2007
- */
-/*
- * Desc: ros sonar controller.
+ * Desc: NPS Ros Sonar controller.
  * Author: Bruce Allen
  * Date: 01 May 2020
  */
@@ -36,7 +15,7 @@
 #include <sdf/sdf.hh>
 #include <sdf/Param.hh>
 #include <gazebo/common/Exception.hh>
-#include <gazebo/sensors/RaySensor.hh>
+#include <gazebo/sensors/SonarSensor.hh>
 #include <gazebo/sensors/SensorTypes.hh>
 #include <gazebo/transport/Node.hh>
 
@@ -47,7 +26,7 @@
 
 #include "gazebo_plugins/nps_gazebo_ros_sonar.h"
 
-#define EPSILON_DIFF 0.000001
+//#define EPSILON_DIFF 0.000001
 
 namespace gazebo
 {
@@ -66,10 +45,10 @@ NpsGazeboRosSonar::~NpsGazeboRosSonar()
 {
   ////////////////////////////////////////////////////////////////////////////////
   // Finalize the controller / Custom Callback Queue
-  this->laser_queue_.clear();
-  this->laser_queue_.disable();
+  this->sonar_queue_.clear();
+  this->sonar_queue_.disable();
   this->rosnode_->shutdown();
-  this->callback_laser_queue_thread_.join();
+  this->callback_sonar_queue_thread_.join();
 
   delete this->rosnode_;
 }
@@ -79,7 +58,7 @@ NpsGazeboRosSonar::~NpsGazeboRosSonar()
 void NpsGazeboRosSonar::Load(sensors::SensorPtr _parent, sdf::ElementPtr _sdf)
 {
   // load plugin
-  RayPlugin::Load(_parent, _sdf);
+  SonarPlugin::Load(_parent, _sdf);
 
   // Get then name of the parent sensor
   this->parent_sensor_ = _parent;
@@ -98,10 +77,10 @@ void NpsGazeboRosSonar::Load(sensors::SensorPtr _parent, sdf::ElementPtr _sdf)
   this->node_->Init(worldName);
 
   GAZEBO_SENSORS_USING_DYNAMIC_POINTER_CAST;
-  this->parent_ray_sensor_ = dynamic_pointer_cast<sensors::RaySensor>(this->parent_sensor_);
+  this->parent_sonar_sensor_ = dynamic_pointer_cast<sensors::SonarSensor>(this->parent_sensor_);
 
-  if (!this->parent_ray_sensor_)
-    gzthrow("NpsGazeboRosSonar controller requires a Ray Sensor as its parent");
+  if (!this->parent_sonar_sensor_)
+    gzthrow("NpsGazeboRosSonar controller requires a Sonar Sensor as its parent");
 
   this->robot_namespace_ = "";
   if (_sdf->HasElement("robotNamespace"))
@@ -117,41 +96,22 @@ void NpsGazeboRosSonar::Load(sensors::SensorPtr _parent, sdf::ElementPtr _sdf)
 
   if (!_sdf->HasElement("topicName"))
   {
-    ROS_INFO_NAMED("sonar", "Sonar plugin missing <topicName>, defaults to /world");
-    this->topic_name_ = "/world";
+    ROS_INFO_NAMED("sonar", "Sonar plugin missing <topicName>, defaults to /range");
+    this->topic_name_ = "/range";
   }
   else
     this->topic_name_ = _sdf->GetElement("topicName")->Get<std::string>();
 
-  if (!_sdf->HasElement("gaussianNoise"))
-  {
-    ROS_INFO_NAMED("sonar", "Sonar plugin missing <gaussianNoise>, defaults to 0.0");
-    this->gaussian_noise_ = 0;
-  }
-  else
-    this->gaussian_noise_ = _sdf->GetElement("gaussianNoise")->Get<double>();
+  this->range_connect_count_ = 0;
 
-  if (!_sdf->HasElement("hokuyoMinIntensity"))
-  {
-    ROS_INFO_NAMED("sonar", "Sonar plugin missing <hokuyoMinIntensity>, defaults to 101");
-    this->hokuyo_min_intensity_ = 101;
-  }
-  else
-    this->hokuyo_min_intensity_ = _sdf->GetElement("hokuyoMinIntensity")->Get<double>();
+  // persistent fields
+  this->range_msg_.header.frame_id = this->frame_name_;
+  // see http://docs.ros.org/melodic/api/sensor_msgs/html/msg/Range.html
+  this->range_msg_.radiation_type = sensor_msgs::Range::ULTRASOUND;
 
-  ROS_DEBUG_NAMED("sonar", "gazebo_ros_laser plugin should set minimum intensity to %f due to cutoff in hokuyo filters." , this->hokuyo_min_intensity_);
-
-  if (!_sdf->HasElement("updateRate"))
-  {
-    ROS_INFO_NAMED("sonar", "Sonar plugin missing <updateRate>, defaults to 0");
-    this->update_rate_ = 0;
-  }
-  else
-    this->update_rate_ = _sdf->GetElement("updateRate")->Get<double>();
-  // FIXME:  update the update_rate_
-
-
-  this->laser_connect_count_ = 0;
+  this->range_msg_.max_range = this->parent_sonar_sensor_->RangeMax();
+  this->range_msg_.min_range = this->parent_sonar_sensor_->RangeMin();
+  this->range_msg_.field_of_view = 0.0;
 
   // Make sure the ROS node for Gazebo has already been initialized
   if (!ros::isInitialized())
@@ -168,18 +128,13 @@ void NpsGazeboRosSonar::Load(sensors::SensorPtr _parent, sdf::ElementPtr _sdf)
   this->rosnode_->getParam(std::string("tf_prefix"), prefix);
   this->frame_name_ = tf::resolve(prefix, this->frame_name_);
 
-  // set size of cloud message, starts at 0!! FIXME: not necessary
-  this->cloud_msg_.points.clear();
-  this->cloud_msg_.channels.clear();
-  this->cloud_msg_.channels.push_back(sensor_msgs::ChannelFloat32());
-
   if (this->topic_name_ != "")
   {
     // Custom Callback Queue
-    ros::AdvertiseOptions ao = ros::AdvertiseOptions::create<sensor_msgs::PointCloud>(
+    ros::AdvertiseOptions ao = ros::AdvertiseOptions::create<sensor_msgs::Range>(
       this->topic_name_,1,
-      boost::bind( &NpsGazeboRosSonar::LaserConnect,this),
-      boost::bind( &NpsGazeboRosSonar::LaserDisconnect,this), ros::VoidPtr(), &this->laser_queue_);
+      boost::bind( &NpsGazeboRosSonar::RangeConnect,this),
+      boost::bind( &NpsGazeboRosSonar::RangeDisconnect,this), ros::VoidPtr(), &this->sonar_queue_);
     this->pub_ = this->rosnode_->advertise(ao);
   }
 
@@ -187,32 +142,32 @@ void NpsGazeboRosSonar::Load(sensors::SensorPtr _parent, sdf::ElementPtr _sdf)
   // Initialize the controller
 
   // sensor generation off by default
-  this->parent_ray_sensor_->SetActive(false);
-  // start custom queue for laser
-  this->callback_laser_queue_thread_ = boost::thread( boost::bind( &NpsGazeboRosSonar::LaserQueueThread,this ) );
+  this->parent_sonar_sensor_->SetActive(false);
+  // start custom queue for sonar
+  this->callback_sonar_queue_thread_ = boost::thread( boost::bind( &NpsGazeboRosSonar::SonarQueueThread,this ) );
 
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Increment count
-void NpsGazeboRosSonar::LaserConnect()
+void NpsGazeboRosSonar::RangeConnect()
 {
-  this->laser_connect_count_++;
-  this->parent_ray_sensor_->SetActive(true);
+  this->range_connect_count_++;
+  this->parent_sonar_sensor_->SetActive(true);
 }
 ////////////////////////////////////////////////////////////////////////////////
 // Decrement count
-void NpsGazeboRosSonar::LaserDisconnect()
+void NpsGazeboRosSonar::RangeDisconnect()
 {
-  this->laser_connect_count_--;
+  this->range_connect_count_--;
 
-  if (this->laser_connect_count_ == 0)
-    this->parent_ray_sensor_->SetActive(false);
+  if (this->range_connect_count_ == 0)
+    this->parent_sonar_sensor_->SetActive(false);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Update the controller
-void NpsGazeboRosSonar::OnNewLaserScans()
+void NpsGazeboRosSonar::OnNewSonarScans()
 {
   if (this->topic_name_ != "")
   {
@@ -225,7 +180,7 @@ void NpsGazeboRosSonar::OnNewLaserScans()
 
     if (last_update_time_ < sensor_update_time)
     {
-      this->PutLaserData(sensor_update_time);
+      this->PutSonarData(sensor_update_time);
       last_update_time_ = sensor_update_time;
     }
   }
@@ -236,174 +191,42 @@ void NpsGazeboRosSonar::OnNewLaserScans()
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-// Put laser data to the interface
-void NpsGazeboRosSonar::PutLaserData(common::Time &_updateTime)
+// Put sonar data to the interface
+void NpsGazeboRosSonar::PutSonarData(common::Time &_updateTime)
 {
-  int i, hja, hjb;
-  int j, vja, vjb;
-  double vb, hb;
-  int    j1, j2, j3, j4; // four corners indices
-  double r1, r2, r3, r4, r; // four corner values + interpolated range
-  double intensity;
+  this->parent_sonar_sensor_->SetActive(false);
 
-  this->parent_ray_sensor_->SetActive(false);
-
-  auto maxAngle = this->parent_ray_sensor_->AngleMax();
-  auto minAngle = this->parent_ray_sensor_->AngleMin();
-
-  double maxRange = this->parent_ray_sensor_->RangeMax();
-  double minRange = this->parent_ray_sensor_->RangeMin();
-  int rayCount = this->parent_ray_sensor_->RayCount();
-  int rangeCount = this->parent_ray_sensor_->RangeCount();
-
-  int verticalRayCount = this->parent_ray_sensor_->VerticalRayCount();
-  int verticalRangeCount = this->parent_ray_sensor_->VerticalRangeCount();
-  auto verticalMaxAngle = this->parent_ray_sensor_->VerticalAngleMax();
-  auto verticalMinAngle = this->parent_ray_sensor_->VerticalAngleMin();
-
-  double yDiff = maxAngle.Radian() - minAngle.Radian();
-  double pDiff = verticalMaxAngle.Radian() - verticalMinAngle.Radian();
-
-
-  // set size of cloud message everytime!
-  //int r_size = rangeCount * verticalRangeCount;
-  this->cloud_msg_.points.clear();
-  this->cloud_msg_.channels.clear();
-  this->cloud_msg_.channels.push_back(sensor_msgs::ChannelFloat32());
-
-  /***************************************************************/
-  /*                                                             */
-  /*  point scan from laser                                      */
-  /*                                                             */
-  /***************************************************************/
-  boost::mutex::scoped_lock sclock(this->lock);
-  // Add Frame Name
-  this->cloud_msg_.header.frame_id = this->frame_name_;
-  this->cloud_msg_.header.stamp.sec = _updateTime.sec;
-  this->cloud_msg_.header.stamp.nsec = _updateTime.nsec;
-
-  for (j = 0; j<verticalRangeCount; j++)
+  // perform sonar scan from Gazebo Sonar sensor
   {
-    // interpolating in vertical direction
-    vb = (verticalRangeCount == 1) ? 0 : (double) j * (verticalRayCount - 1) / (verticalRangeCount - 1);
-    vja = (int) floor(vb);
-    vjb = std::min(vja + 1, verticalRayCount - 1);
-    vb = vb - floor(vb); // fraction from min
+    boost::mutex::scoped_lock sclock(this->lock);
 
-    assert(vja >= 0 && vja < verticalRayCount);
-    assert(vjb >= 0 && vjb < verticalRayCount);
+    // Add Frame Name
+    this->range_msg_.header.frame_id = this->frame_name_;
+    this->range_msg_.header.stamp.sec = _updateTime.sec;
+    this->range_msg_.header.stamp.nsec = _updateTime.nsec;
 
-    for (i = 0; i<rangeCount; i++)
-    {
-      // Interpolate the range readings from the rays in horizontal direction
-      hb = (rangeCount == 1)? 0 : (double) i * (rayCount - 1) / (rangeCount - 1);
-      hja = (int) floor(hb);
-      hjb = std::min(hja + 1, rayCount - 1);
-      hb = hb - floor(hb); // fraction from min
+    // range
+    double range = this->parent_sonar_sensor_->Range();
+    this->range_msg_.range = range;
 
-      assert(hja >= 0 && hja < rayCount);
-      assert(hjb >= 0 && hjb < rayCount);
+    this->parent_sonar_sensor_->SetActive(true);
 
-      // indices of 4 corners
-      j1 = hja + vja * rayCount;
-      j2 = hjb + vja * rayCount;
-      j3 = hja + vjb * rayCount;
-      j4 = hjb + vjb * rayCount;
-      // range readings of 4 corners
-      r1 = std::min(this->parent_ray_sensor_->LaserShape()->GetRange(j1) , maxRange-minRange);
-      r2 = std::min(this->parent_ray_sensor_->LaserShape()->GetRange(j2) , maxRange-minRange);
-      r3 = std::min(this->parent_ray_sensor_->LaserShape()->GetRange(j3) , maxRange-minRange);
-      r4 = std::min(this->parent_ray_sensor_->LaserShape()->GetRange(j4) , maxRange-minRange);
-
-      // Range is linear interpolation if values are close,
-      // and min if they are very different
-      r = (1-vb)*((1 - hb) * r1 + hb * r2)
-         +   vb *((1 - hb) * r3 + hb * r4);
-
-      // Intensity is averaged
-      intensity = 0.25*(this->parent_ray_sensor_->LaserShape()->GetRetro(j1) +
-                        this->parent_ray_sensor_->LaserShape()->GetRetro(j2) +
-                        this->parent_ray_sensor_->LaserShape()->GetRetro(j3) +
-                        this->parent_ray_sensor_->LaserShape()->GetRetro(j4));
-
-      // std::cout << " sonar debug "
-      //           << "  ij("<<i<<","<<j<<")"
-      //           << "  j1234("<<j1<<","<<j2<<","<<j3<<","<<j4<<")"
-      //           << "  r1234("<<r1<<","<<r2<<","<<r3<<","<<r4<<")"
-      //           << std::endl;
-
-      // get angles of ray to get xyz for point
-      double yAngle = 0.5*(hja+hjb) * yDiff / (rayCount -1) + minAngle.Radian();
-      double pAngle = 0.5*(vja+vjb) * pDiff / (verticalRayCount -1) + verticalMinAngle.Radian();
-
-      /***************************************************************/
-      /*                                                             */
-      /*  point scan from laser                                      */
-      /*                                                             */
-      /***************************************************************/
-      //compare 2 doubles
-      double diffRange = maxRange - minRange;
-      double diff  = diffRange - r;
-      if (fabs(diff) < EPSILON_DIFF)
-      {
-        // no noise if at max range
-        geometry_msgs::Point32 point;
-        //pAngle is rotated by yAngle:
-        point.x = r * cos(pAngle) * cos(yAngle);
-        point.y = r * cos(pAngle) * sin(yAngle);
-        point.z = r * sin(pAngle);
-
-        this->cloud_msg_.points.push_back(point);
-      }
-      else
-      {
-        geometry_msgs::Point32 point;
-        //pAngle is rotated by yAngle:
-        point.x = r * cos(pAngle) * cos(yAngle) + this->GaussianKernel(0,this->gaussian_noise_);
-        point.y = r * cos(pAngle) * sin(yAngle) + this->GaussianKernel(0,this->gaussian_noise_);
-        point.z = r * sin(pAngle) + this->GaussianKernel(0,this->gaussian_noise_);
-        this->cloud_msg_.points.push_back(point);
-      } // only 1 channel
-
-      this->cloud_msg_.channels[0].values.push_back(intensity + this->GaussianKernel(0,this->gaussian_noise_)) ;
-    }
+    // send data out via ros message
+    if (this->range_connect_count_ > 0 && this->topic_name_ != "")
+        this->pub_.publish(this->range_msg_);
   }
-  this->parent_ray_sensor_->SetActive(true);
-
-  // send data out via ros message
-  this->pub_.publish(this->cloud_msg_);
-
-
-
-}
-
-
-//////////////////////////////////////////////////////////////////////////////
-// Utility for adding noise
-double NpsGazeboRosSonar::GaussianKernel(double mu,double sigma)
-{
-  // using Box-Muller transform to generate two independent standard normally disbributed normal variables
-  // see wikipedia
-  double U = (double)rand()/(double)RAND_MAX; // normalized uniform random variable
-  double V = (double)rand()/(double)RAND_MAX; // normalized uniform random variable
-  double X = sqrt(-2.0 * ::log(U)) * cos( 2.0*M_PI * V);
-  //double Y = sqrt(-2.0 * ::log(U)) * sin( 2.0*M_PI * V); // the other indep. normal variable
-  // we'll just use X
-  // scale to our mu and sigma
-  X = sigma * X + mu;
-  return X;
 }
 
 // Custom Callback Queue
 ////////////////////////////////////////////////////////////////////////////////
 // custom callback queue thread
-void NpsGazeboRosSonar::LaserQueueThread()
+void NpsGazeboRosSonar::SonarQueueThread()
 {
   static const double timeout = 0.01;
 
   while (this->rosnode_->ok())
   {
-    this->laser_queue_.callAvailable(ros::WallDuration(timeout));
+    this->sonar_queue_.callAvailable(ros::WallDuration(timeout));
   }
 }
 
@@ -415,6 +238,5 @@ void NpsGazeboRosSonar::OnStats( const boost::shared_ptr<msgs::WorldStatistics c
   pose.Pos().X() = 0.5*sin(0.01*this->sim_time_.Double());
   gzdbg << "plugin simTime [" << this->sim_time_.Double() << "] update pose [" << pose.Pos().X() << "]\n";
 }
+} // namespace
 
-
-}
